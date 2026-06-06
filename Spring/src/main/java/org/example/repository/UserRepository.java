034d964f7c4848724d7377ef647bec97eeca9674
mariakirnitskaya <maria105@mail.ru>
2026-06-06 18:06:44 +0700

    Spring App added

---
+++ Spring/src/main/java/org/example/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.example.repository;
+
+import org.example.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, Integer id);
+}
